60f7185880f1788a9691188166dc2c6f2982f894
Saaquib Bokhari <bokharisaab12345@gmail.com>
2024-09-24 15:31:20 -0400

    formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    //check if user has visited the home page through cookies (current session)
-    if (!sessionStorage.getItem('hasVisited')){
-        //if they haven't, play the animation
-        document.getElementById('heading').classList.add('fade-fast');
-        document.getElementById('subheading').classList.add('fade-delayed');
-        document.getElementById('links').classList.add('fade-delayed');
+document.addEventListener("DOMContentLoaded", () => {
+  //check if user has visited the home page through cookies (current session)
+  if (!sessionStorage.getItem("hasVisited")) {
+    //if they haven't, play the animation
+    document.getElementById("heading").classList.add("fade-fast");
+    document.getElementById("subheading").classList.add("fade-delayed");
+    document.getElementById("links").classList.add("fade-delayed");
 
-        // //set a flag to mark the page as already visited to prevent the animation to keep replaying
-        sessionStorage.setItem('hasVisited', 'true');
-    }
+    // //set a flag to mark the page as already visited to prevent the animation to keep replaying
+    sessionStorage.setItem("hasVisited", "true");
+  }
 });
